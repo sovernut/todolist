055dfe8e5b32b9eeeac546802909643eb3695137
Nuttaphon <nattaphon@sirisoft.co.th>
2019-09-14 13:24:13 +0700

    add longpress

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,42 +9,62 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class HomePage {
   formTodo: FormGroup
   todolist = [
-    {todo: 'Todo#1',
-    priority: '!!!',
-    date: '13/09/2019',
-    done: false},
-    {todo: 'Todo#2',
-    priority: '!!',
-    date: '12/09/2019',
-    done: false}
+    {
+      todo: 'Todo#1',
+      priority: '!!!',
+      date: '13/09/2019',
+      done: false
+    },
+    {
+      todo: 'Todo#2',
+      priority: '!!',
+      date: '12/09/2019',
+      done: false
+    }
   ]
   isAdding: Boolean = false
+  longPress: Boolean[] = []
+
   constructor(
     public navCtrl: NavController,
     private formBuilder: FormBuilder
-    ) {
-      this.formTodo = this.formBuilder.group({
-        todo:'',
-        priority: '!!!',
-        date: '11/09/2019'
-      });
-      // this.formTodo.controls['todolistForm'].
+  ) {
+    this.formTodo = this.formBuilder.group({
+      todo: '',
+      priority: '!!!',
+      date: '11/09/2019'
+    });
+    let itemlen = 3
+    for (let i = 0; i < itemlen; i++) {
+      this.longPress.push(false)
+    }
+    // this.formTodo.controls['todolistForm'].
   }
 
-  onTodoChange(){
+  onTodoChange() {
     console.log(this.formTodo.controls['todo'].value)
   }
 
-  onAddForm(){
+  onAddForm() {
     this.isAdding = true;
     // this.formTodo.controls['todolistForm'].push(this.formTodoTemp)
   }
 
-  onLostFocus(){
+  onLostFocus() {
     this.isAdding = false;
   }
 
-  editTodo(){
-    console.log('ling press')
+  editTodo(i) {
+    console.log('long press')
+    this.longPress[i] = true
+  }
+
+
+
+  doneTodo(i) {
+    if (!this.longPress[i]) {
+      console.log('done')
+    }
+    this.longPress[i] = false;
   }
 }
